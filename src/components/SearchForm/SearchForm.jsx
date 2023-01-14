dacee69968e31794bebacd2abf408eea6a6b5e8c
Natalie Tkachenko <tkachenko_nataliya83@ukr.net>
2023-01-14 19:47:23 +0200

    task 1

---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,38 @@
+import { Component } from 'react';
+
+import { FiSearch } from 'react-icons/fi';
+import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
+
+export class SearchForm extends Component {
+  state = {
+    query: '',
+  };
+
+  handleInput = event => {
+    this.setState({ query: event.target.value.toLowerCase().trim() });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.query);
+    this.setState({ query: '' });
+  };
+
+  render() {
+    return (
+      <SearchFormStyled onSubmit={this.handleSubmit}>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          placeholder="What do you want to write?"
+          name="search"
+          value={this.state.query}
+          required
+          autoFocus
+          onChange={this.handleInput}
+        />
+      </SearchFormStyled>
+    );
+  }
+}
